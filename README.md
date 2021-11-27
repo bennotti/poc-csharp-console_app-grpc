513232f6f269aaf457706b777733c02eb9de5d40
Bruno Bennotti B Moura <brunobennotti@gmail.com>
2021-11-27 15:55:18 -0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## poc-csharp-console_app-grpc
+
+### Atalhos VsCode
+
+- Markdown Preview: crtl+shift+v
+
+### Dependencias
+
+- .net core 3.1
+- Grpc
---
+++ src/SampleProjectGrpcClient/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace SampleProjectGrpcClient
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Grpc.Core.Channel channel = new Grpc.Core.Channel("127.0.0.1", 5000, ChannelCredentials.Insecure);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
